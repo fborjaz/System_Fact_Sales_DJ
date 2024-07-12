840c7541b09f223bc917c3c7bf7f49543fe82bbd
fborjaz <fborjaz@unemi.edu.ec>
2024-07-12 04:30:57 -0500

    chore: Update modulos.html to add top margin to section

--- app/core/views/modulos.py
+++ app/core/views/modulos.py
@@ -11,7 +11,7 @@ from django.contrib import messages
 class ModuloTemplateView(PermissionMixin,TemplateView):
     template_name = 'components/modulos.html'
    
-    def get_context_data(self, **kwargs): #agregar datos adicionales
+    def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["title1"]= "IC - Modulos"
         context["title2"]= "Modulos Disponibles"
