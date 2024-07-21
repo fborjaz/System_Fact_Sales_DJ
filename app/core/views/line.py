ab243d60a4cb12f1a81d2e297bb698fb104082e6
fborjaz <fborjaz@unemi.edu.ec>
2024-07-21 11:55:40 -0500

    Coreccion de view line

--- app/core/views/line.py
+++ app/core/views/line.py
@@ -3,7 +3,7 @@ from django.urls import reverse_lazy
 from app.core.forms.supplier import LineForm 
 from app.core.models import Line
 from app.security.instance.menu_module import MenuModule
-from app.security.mixins.mixins import CreateViewMixin, ListViewMixin, PermissionMixin, UpdateViewMixin
+from app.security.mixins.mixins import CreateViewMixin, ListViewMixin, PermissionMixin, UpdateViewMixin, DeleteViewMixin
 from django.views.generic import CreateView, ListView, UpdateView, DeleteView
 from django.contrib import messages
 from django.db.models import Q
@@ -12,43 +12,36 @@ from django.db.models import Q
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # -- LINE CLASS --#
-class LineListView(ListViewMixin, ListView):
-    template_name = 'core/lines/list.html'
+class LineListView(PermissionMixin, ListViewMixin, ListView):
     model = Line
+    template_name = 'core/lines/list.html'
     context_object_name = "lines"
     permission_required = "view_line"
+    paginate_by = 10
     
     def get_queryset(self):
-        self.query = Q(active=True)
-        q1 = self.request.GET.get('q')
-        print()
-        if q1 is not None:
-            self.query.add(Q(description__icontains=q1), Q.OR)
+        self.query = Q()
+        q = self.request.GET.get('q')
+        if q:
+            self.query &= Q(description__icontains=q)
         return self.model.objects.filter(self.query).order_by("id")
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        queryset = self.get_queryset()
-        paginator = Paginator(queryset, self.paginate_by)
-
-        page = self.request.GET.get('page')
-        try:
-            line = paginator.page(page)
-        except PageNotAnInteger:
-            line = paginator.page(1)
-        except EmptyPage:
-            line = paginator.page(paginator.num_pages)
-
-        context['lines'] = line
-        context['title1'] = 'IC - Lineas'
-        context['title2'] = 'Consulta de lineas'
+        lines = self.get_queryset()
+        paginator = Paginator(lines, self.paginate_by)
+        page_number = self.request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
+        context['lines'] = page_obj
+        context['title1'] = 'IC - Líneas'
+        context['title2'] = 'Consulta de líneas'
         context['create_url'] = reverse_lazy('core:line_create')
         context['query'] = self.request.GET.get('q', '')
         return context
 
-    
 
-class LineCreateView(CreateViewMixin, CreateView):
+
+class LineCreateView(PermissionMixin, CreateViewMixin, CreateView):
     model = Line
     template_name = 'core/lines/form.html'
     form_class = LineForm
@@ -63,7 +56,7 @@ class LineCreateView(CreateViewMixin, CreateView):
         context['back_url'] = self.success_url
         return context
 
-class LineUpdateView(UpdateViewMixin, UpdateView):
+class LineUpdateView(PermissionMixin, UpdateViewMixin, UpdateView):
     model = Line
     template_name = 'core/lines/form.html'
     form_class = LineForm
